034b2d56b49aeb45f5c87b862baa214ab1e9be2c
TrevorZylks <trevorzylks@gmail.com>
2021-08-10 11:12:02 -0700

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+  if self.end_with?('.')
+      true
+  else
+    false
+  end
   end
 
   def question?
-
+    if self.end_with?('?')
+      true
+  else
+    false
+  end
   end
 
   def exclamation?
-
+    if self.end_with?('!')
+      true
+  else
+    false
+  end
   end
 
   def count_sentences
-
+    self.size
   end
 end
+
+puts 'hi.'.sentence?
+puts 'hi how are you'.count_sentences
